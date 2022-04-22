9a49459ea5fcdbe1f36cabf916002dd2befce760
JavierPolaris <javierrojocanton@gmail.com>
2022-04-22 17:17:53 +0200

    empiezo una calculadora

---
+++ Ejercicios_JS/codigoSuma.js
@@ -0,0 +1,22 @@
+var operacion;
+document.querySelector('#suma').addEventListener('click', () => {
+    operacion = '+';
+});
+document.querySelector('#resta').addEventListener('click', () => {
+    operacion = '-';
+});
+
+document.querySelector('#boton').addEventListener('click', () => {
+    const n1 = parseInt(document.querySelector('#numero1').value);
+    const n2 = parseInt(document.querySelector('#numero2').value);
+
+    let resultado;
+    if(operacion == '+'){
+        resultado = numero1 + numero2;
+    }else if(operacion == '-') {
+        resultado = numero1 - numero2;
+    }else{
+        alert("que estas haciendo inutil!!!")
+    }
+    document.querySelector('#resultado').innerHTML = resultado;
+});
