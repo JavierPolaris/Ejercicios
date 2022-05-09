1f0b6bca6c8e038950b1d2b85f8f86de867c2045
JavierPolaris <javierrojocanton@gmail.com>
2022-05-09 20:21:50 +0200

    intento pintar de vomito el otro lado

--- Ejercicios_JS/EjerciciosPOO/js/codigo.js
+++ Ejercicios_JS/EjerciciosPOO/js/codigo.js
@@ -83,50 +83,64 @@
  document.getElementById('btn').addEventListener('click', (e) => {
    e.preventDefault();
    
-    let dimension1 = 4
-    let dimension2 = 10
-    let total = "";
-    let space = dimension1 - 1;
+   let dimension1 = 4
+   let dimension2 = 10
+   let total = "";
+   let space = dimension1 - 1;
+   
    // Creamos la parte superior desde dimension1 hasta dimension2, pero incrementamos de 2 en 2
-    for (let i = dimension1; i < dimension2; i+= 2) {
-        // Dibujamos los espacios en cada ciclo
-        for (let j = 0; j < space; j++) {
-            total += '-';   
-        }
-         // Decrementamos la variable de espacios
-        space--
-        // Dibujamos los "+" desde 0 hasta el valor actual de i, que sería desde dimension1 hasta dimension2
-        for (let j = 0; j < i; j++) {
-            total += '+';  
-        }
-        // Insertamos un salto de línea
+   for (let i = dimension1; i <= (dimension2); i += 2){
+           // Dibujamos los espacios en cada ciclo
+           for (let j = 0; j < space; j++){
+               total += '🤮';
+       }
+       // Decrementamos la variable de espacios
+       space--;
+       // Dibujamos los "+" desde 0 hasta el valor actual de i, que sería desde dimension1 hasta dimension2
+       for (let j = 0; j < i; j++){
+               total += '💩';
+       }
+       // Insertamos un salto de línea
        total += '\n'
-        
-    }
-  
-    // Inicializamos la variable de espacios en 1 ya que la parte inferior requiere un espacio en el ciclo inicial
-    space = 1;
-    // Dibujamos la parte inferior desde dimension2-2 hasta dimension1 pero decrementamos de 2 en 2
-    for (let i = dimension2 - 2; i >= dimension1; i-= 2) {
-         // Dibujamos los espacios en cada ciclo
-        for (let j = 0; j < space; j++) {
-            total += '-';   
-             
-        }
-        // Incrementamos la variable de espacios
-        space++
-        // Dibujamos los "+" desde 0 hasta el valor actual de i, que sería desde dimension2 hasta dimension1
-        for (let j = 0; j < i; j++) {
-            total += '+';            
-        }
-        
+   }
+   // Inicializamos la variable de espacios en 1 ya que la parte inferior requiere un espacio en el ciclo inicial
+   space = 1;
+   // Dibujamos la parte inferior desde dimension2-2 hasta dimension1 pero decrementamos de 2 en 2
+   for (let i = dimension2-2; i >= (dimension1); i -= 2){
+           // Dibujamos los espacios en cada ciclo
+           for (let j = 0; j < space; j++){
+               total += '🤮';
+       }
+       // Incrementamos la variable de espacios
+       space++;
+       // Dibujamos los "+" desde 0 hasta el valor actual de i, que sería desde dimension2 hasta dimension1
+       for (let j = 0; j < i; j++){
+               total += '💩';
+       }
        total += '\n'
-        
-    }
-    alert(total)
+   }
+   
+   // Dibujamos el rombo
+   alert(total)
+   console.log(alert)
 })
+    // for (let k = 0; k < (dimension2) - 1; k++) {
+    //     for (let c = 1; c < space; c++) {
+    //         total += ' ';
+    //     }
+    //     space ++;
+    //     for (let c = 0; c < dimension2 + (k * 2); c++) {
+    //         total += '+';
+    //     }
+    //     total += '\n'
+    // }
     
     
+   
+   
+
+
+
     // function diamond(l, i = 4) {
         
     //     const MAS = '+';
