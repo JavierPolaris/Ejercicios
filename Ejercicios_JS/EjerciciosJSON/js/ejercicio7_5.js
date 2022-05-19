d4bc1520915dd04067d026fde9c392e890e99c7b
JavierPolaris <javierrojocanton@gmail.com>
2022-05-19 11:50:30 +0200

    primera parte del trabajo en grupo terminado

--- Ejercicios_JS/EjerciciosJSON/js/ejercicio7_5.js
+++ Ejercicios_JS/EjerciciosJSON/js/ejercicio7_5.js
@@ -7,74 +7,124 @@
 // 1       Hola      3
 // Adiós   3         2   -> Muestra Hola y Manzana
 // 4       Manzana   5
- document.getElementById('boton').addEventListener('click', (e) => {
-     e.preventDefault();
-    var frase = document.getElementById('entrada').value;
-    var nuevaFrase = frase.split(" ");
-    let p1 = [];
-    let p2 = [];
-    let p3 = [];
-    let p4 = [];
-    let p5 = [];
-    let p6 = [];
-    let p7 = [];
-    let p8 = [];
-    let p9 = [];
-    let cont = 0;
-    for (let i = 0; i < nuevaFrase.length; i++) {
-        if (nuevaFrase[i] == nuevaFrase[0]) {
-           p1.push(nuevaFrase[0])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[1]){
-            p2.push(nuevaFrase[1])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[2]){
-            p3.push(nuevaFrase[2])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[3]){
-            p4.push(nuevaFrase[3])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[4]){
-            p5.push(nuevaFrase[4])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[5]){
-            p6.push(nuevaFrase[5])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[6]){
-            p7.push(nuevaFrase[6])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[7]){
-            p8.push(nuevaFrase[7])
-           cont++
-        }else if (nuevaFrase[i] == nuevaFrase[8]){
-            p9.push(nuevaFrase[8])
-           cont++
-        }else{
-            alert('Maximo 9 palabras')
-        }
+//  document.getElementById('boton').addEventListener('click', (e) => {
+//      e.preventDefault();
+//     var frase = document.getElementById('entrada').value;
+//     var nuevaFrase = frase.split(" ");
+//     let p1 = [];
+//     let p2 = [];
+//     let p3 = [];
+//     let p4 = [];
+//     let p5 = [];
+//     let p6 = [];
+//     let p7 = [];
+//     let p8 = [];
+//     let p9 = [];
+//     let cont = 0;
+//     for (let i = 0; i < nuevaFrase.length; i++) {
+//         if (nuevaFrase[i] == nuevaFrase[0]) {
+//            p1.push(nuevaFrase[0])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[1]){
+//             p2.push(nuevaFrase[1])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[2]){
+//             p3.push(nuevaFrase[2])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[3]){
+//             p4.push(nuevaFrase[3])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[4]){
+//             p5.push(nuevaFrase[4])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[5]){
+//             p6.push(nuevaFrase[5])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[6]){
+//             p7.push(nuevaFrase[6])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[7]){
+//             p8.push(nuevaFrase[7])
+//            cont++
+//         }else if (nuevaFrase[i] == nuevaFrase[8]){
+//             p9.push(nuevaFrase[8])
+//            cont++
+//         }else{
+//             alert('Maximo 9 palabras')
+//         }
+//     }
+
+
+// var nuevoArray = new Array(3);
+// nuevoArray[0] = new Array(3);
+// nuevoArray[1] = new Array(3);
+// nuevoArray[2] = new Array(3);
+
+// //TODO Metemos un dato en cada posición:
+// nuevoArray[0][0] = p1;
+// nuevoArray[0][1] = p2;
+// nuevoArray[0][2] = p3;
+// nuevoArray[1][0] = p4;
+// nuevoArray[1][1] = p5;
+// nuevoArray[1][2] = p6;
+// nuevoArray[2][0] = p7;
+// nuevoArray[2][1] = p8;
+// nuevoArray[2][2] = p9;
+
+
+// document.getElementById('salida').innerText = `los nuemeros son ${nuevoArray[0][1]} y ${nuevoArray[2][1]}`
+// console.log(`${nuevoArray[0][1]} ${nuevoArray[2][1]}`)
+// })
+
+
+//TODO correcion Davinia
+let arrayBid = [];
+const pedirFila = (fila) => {
+  let datos = [];
+  let columnas;
+  do {
+    columnas = Number(prompt('Inserte número de columnas'));
+    if (columnas <= 0) {
+      alert('El número debe ser mayor que cero');
     }
+  } while (columnas <= 0);
+  for (let i = 0; i < columnas; i++) {
+    datos.push(prompt(`Inserta el elemento ${fila}x${i}`));
+  }
+  return datos;
+};
+function pedirMatriz() {
+  let filas;
+  do {
+    filas = Number(prompt('Inserte número de filas'));
+    if (filas <= 0) {
+      alert('El número debe ser mayor que cero');
+    }
+  } while (filas <= 0);
+  for (let i = 0; i < filas; i++) {
+    arrayBid.push(pedirFila(i));
+  }
+  return arrayBid;
+}
+function validarMatriz(arrayBid) {
+  console.log(arrayBid);
+  for (let i = 0; i < arrayBid.length; i += 2) {
+    for (let j = 1; j < arrayBid[i].length; j += 2) {
+      if (typeof arrayBid[i][j] == 'string') {
+        console.log(arrayBid[i][j]);
+      }
+    }
+  }
+}
+// pedirFila(0);
+// pedirMatriz();
+validarMatriz(pedirMatriz());
 
 
-var nuevoArray = new Array(3);
-nuevoArray[0] = new Array(3);
-nuevoArray[1] = new Array(3);
-nuevoArray[2] = new Array(3);
-
-//TODO Metemos un dato en cada posición:
-nuevoArray[0][0] = p1;
-nuevoArray[0][1] = p2;
-nuevoArray[0][2] = p3;
-nuevoArray[1][0] = p4;
-nuevoArray[1][1] = p5;
-nuevoArray[1][2] = p6;
-nuevoArray[2][0] = p7;
-nuevoArray[2][1] = p8;
-nuevoArray[2][2] = p9;
 
 
-document.getElementById('salida').innerText = `los nuemeros son ${nuevoArray[0][1]} y ${nuevoArray[2][1]}`
-console.log(`${nuevoArray[0][1]} ${nuevoArray[2][1]}`)
-})
+
+
 // Crea un array a partir de las siguientes instrucciones:
 // El tamaño es solicitado al usuario
 // Elemento1: 1
